ea62d987e4b5d13a0c5c7083a640c2c0a124eedb
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-16 07:34:56 +0200

    we can simplify this actually

--- src/Plugin.php
+++ src/Plugin.php
@@ -21,26 +21,17 @@ class Plugin extends Procedures\Plugin
         $stmt->execute([$this->loader->getUser()]);
         $uid = $stmt->fetchColumn();
         // Source: http://stackoverflow.com/questions/7622908/drop-function-without-knowing-the-number-type-of-parameters
-        $format = $this->loader->getPdo()->prepare(
-            "SELECT format('DROP FUNCTION %s(%s) CASCADE;',
+        $existing = $this->loader->getPdo()->prepare(
+            "SELECT proname, format('DROP FUNCTION %s(%s) CASCADE;',
                 oid::regproc,
                 pg_get_function_identity_arguments(oid)) the_query
             FROM pg_proc
-                WHERE proname = ?
-                AND pg_function_is_visible(oid)
+                WHERE pg_function_is_visible(oid)
                 AND proowner = ?");
-        $existing = $this->loader->getPdo()->prepare("SELECT
-                ROUTINE_TYPE routinetype,
-                ROUTINE_NAME routinename
-            FROM INFORMATION_SCHEMA.ROUTINES WHERE
-                (ROUTINE_CATALOG = ? OR ROUTINE_SCHEMA = ?)");
-        $existing->execute([$this->loader->getDatabase(), $this->loader->getDatabase()]);
+        $existing->execute([$uid]);
         while (false !== ($routine = $existing->fetch(PDO::FETCH_ASSOC))) {
-            if (!$this->loader->shouldBeIgnored($routine['routinename'])) {
-                $format->execute([$routine['routinename'], $uid]);
-                while ($query = $format->fetchColumn()) {
-                    $this->addOperation($query);
-                }
+            if (!$this->loader->shouldBeIgnored($routine['proname'])) {
+                $this->addOperation($routine['the_query']);
             }
         }
     }
